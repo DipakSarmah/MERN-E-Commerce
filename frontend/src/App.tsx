be78a804651b9a7c817d8cf10c984716d0cbb061
dipak sarmah <sarmahd60@gmail.com>
2024-02-16 23:40:02 +0530

    added react-router-dom and created homepage and productpage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
-import { sampleProducts } from './data'
-import { Container, Col, Row, Navbar, Nav } from 'react-bootstrap'
-
+import { Container, Navbar, Nav } from 'react-bootstrap'
+import { Outlet } from 'react-router-dom'
 function App() {
   return (
     <div className="d-flex flex-column vh-100">
@@ -20,19 +19,7 @@ function App() {
         </Navbar>
       </header>
       <Container className="mt-3">
-        <Row>
-          {sampleProducts.map((product) => (
-            <Col key={product.slug} sm={6} md={4} lg={3}>
-              <img
-                className="product-image"
-                src={product.image}
-                alt={product.name}
-              />
-              <h2>{product.name}</h2>
-              <p>${product.price}</p>
-            </Col>
-          ))}
-        </Row>
+        <Outlet />
       </Container>
 
       <footer>
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,8 +3,29 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from 'react-router-dom'
+import HomePage from './pages/HomePage.tsx'
+import ProductPage from './pages/ProductPage.tsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} element={<HomePage />} />
+      <Route path="product/:id" element={<ProductPage />} />
+
+      {/* ... etc. */}
+    </Route>
+  )
+);
+
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router = {router}/>
   </React.StrictMode>
 )
