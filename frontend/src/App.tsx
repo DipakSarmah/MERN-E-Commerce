db65570456bc84ddd44b1c58d0558988315518b8
dipak sarmah <sarmahd60@gmail.com>
2024-02-16 23:17:10 +0530

    added bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,28 @@
-import './App.css'
 import { sampleProducts } from './data'
+import { Container, Col, Row, Navbar, Nav } from 'react-bootstrap'
 
 function App() {
   return (
-    <div>
-      <header>TS Amazona</header>
-      <main>
-        <ul>
+    <div className="d-flex flex-column vh-100">
+      <header>
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand>Amazone</Navbar.Brand>
+          </Container>
+          <Nav>
+            <a href="/cart" className="nav-link">
+              Cart
+            </a>
+            <a href="/signin" className="nav-link">
+              Signin
+            </a>
+          </Nav>
+        </Navbar>
+      </header>
+      <Container className="mt-3">
+        <Row>
           {sampleProducts.map((product) => (
-            <li key={product.slug}>
+            <Col key={product.slug} sm={6} md={4} lg={3}>
               <img
                 className="product-image"
                 src={product.image}
@@ -16,12 +30,14 @@ function App() {
               />
               <h2>{product.name}</h2>
               <p>${product.price}</p>
-            </li>
+            </Col>
           ))}
-        </ul>
-      </main>
+        </Row>
+      </Container>
 
-      <footer>All right reserved</footer>
+      <footer>
+        <div className="text-center">All right reserved</div>
+      </footer>
     </div>
   )
 }
