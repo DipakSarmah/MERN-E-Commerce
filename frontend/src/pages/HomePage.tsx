4d943f6a1e3b869728debea496dedd89394b6071
dipak sarmah <sarmahd60@gmail.com>
2024-02-20 17:10:28 +0530

    added ProductItem component

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -8,6 +8,7 @@ import axios from 'axios'
 import { getError } from '../utils'
 import LoadingBox from '../components/LoadingBox'
 import MessageBox from '../components/MessageBox'
+import ProductItem from '../components/ProductItem'
 
 type State = {
   products: Product[]
@@ -70,15 +71,7 @@ function HomePage() {
     <Row>
       {products.map((product) => (
         <Col key={product.slug} sm={6} md={4} lg={3}>
-          <Link to={'/product/' + product.slug}>
-            <img
-              className="product-image"
-              src={product.image}
-              alt={product.name}
-            />
-            <h2>{product.name}</h2>
-            <p>${product.price}</p>
-          </Link>
+          <ProductItem product={product} />
         </Col>
       ))}
     </Row>
