5e82cb20c914727ed59a77649451d64b7632bb2d
dipak sarmah <sarmahd60@gmail.com>
2024-02-23 18:18:18 +0530

    added addToCart functionality in addToCart button

--- frontend/src/Store.tsx
+++ frontend/src/Store.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Cart, CartItem } from './types/Card'
 
 type AppState = {
   mode: string
+  cart: Cart
 }
 
+// since object are store in localstore as a string
+// we need to convert string to object using JSON.parse(anyString)
 const initialState: AppState = {
   mode: localStorage.getItem('mode')
     ? localStorage.getItem('mode')!
@@ -11,16 +15,49 @@ const initialState: AppState = {
       window.matchMedia('prefers-color-scheme: dark').matches
     ? 'dark'
     : 'light',
+
+  cart: {
+    cartItems: localStorage.getItem('cartItems')
+      ? JSON.parse(localStorage.getItem('cartItems')!)
+      : [],
+    shippingAddress: localStorage.getItem('shippingAddress')
+      ? JSON.parse(localStorage.getItem('shippingAddress')!)
+      : {},
+    paymentMethod: localStorage.getItem('paymentMethod')
+      ? localStorage.getItem('paymentMethod')!
+      : 'PayPal',
+    itemsPrice: 0,
+    shippingPrice: 0,
+    taxPrice: 0,
+    totalPrice: 0,
+  },
 }
 
-type Action = {
-  type: 'SWITCH_MODE'
-}
+type Action =
+  | { type: 'SWITCH_MODE' }
+  | { type: 'CART_ADD_ITEM'; payload: CartItem }
 
 function reducer(state: AppState, action: Action): AppState {
   switch (action.type) {
     case 'SWITCH_MODE':
-      return { mode: state.mode === 'dark' ? 'light' : 'dark' }
+      return { ...state, mode: state.mode === 'dark' ? 'light' : 'dark' }
+    case 'CART_ADD_ITEM':
+      const newItem = action.payload
+
+      const existItem = state.cart.cartItems.find(
+        (item: CartItem) => item._id === newItem._id
+      )
+
+      const cartItems = existItem
+        ? state.cart.cartItems.map((item: CartItem) =>
+            item._id === existItem._id ? newItem : item
+          )
+        : [...state.cart.cartItems, newItem]
+
+      localStorage.setItem('cartItems', JSON.stringify(cartItems))
+
+      return { ...state, cart: { ...state.cart, cartItems } }
+
     default:
       return state
   }
@@ -40,6 +77,5 @@ function StoreProvider(props: React.PropsWithChildren<{}>) {
   )
   return <Store.Provider value={{ state, dispatch }} {...props} />
 }
-//
-//
+
 export { Store, StoreProvider }
